9c238cd9f423c9146675e16df9101487a1d3bed6
Damian Suess <djsuess@gmail.com>
2022-07-25 18:46:27 -0400

    Moved models to models folder. Created test bed for GattService

---
+++ src/Plugin.BlueZ.ManualTest/GattServer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Plugin.BlueZ.ManualTest
+{
+  /// <summary>Gatt Server - COMING SOON.</summary>
+  /// <remarks>
+  ///   References:
+  ///   - https://github.com/bluez/bluez/blob/master/test/example-gatt-server
+  /// </remarks>
+  public class GattServer
+  {
+    // 1. BlueZManager.GetAdapterAsync();
+    // 2. CreateProxy GATT Description
+    // 3. Create GATT Services
+    // 4. Create GATT Characteristics
+
+    /*
+     * 'org.bluez'
+     * 'org.bluez.GattManager1'
+     * 'org.freedesktop.DBus.ObjectManager'
+     * 'org.freedesktop.DBus.Properties'
+     * 
+     * 'org.bluez.GattService1'
+     * 'org.bluez.GattCharacteristic1'
+     * 'org.bluez.GattDescriptor1'
+     */
+  }
+}
--- src/Plugin.BlueZ.Tests/AdapterTests.cs
+++ src/Plugin.BlueZ.Tests/AdapterTests.cs
@@ -10,36 +10,40 @@ namespace Plugin.BlueZ.Tests
   [TestClass]
   public class AdapterTests
   {
-    [TestMethod]
-    public async Task BasicTest()
-    {
-      // Test Server
-      var server = new ServerConnectionOptions();
-      using (var connection = new Connection(server))
-      {
-        await connection.RegisterObjectAsync(new Adapter());
-        var boundAddress = await server.StartAsync("tcp:host=localhost");
-
-        Console.WriteLine($"Server is listening at, {boundAddress}");
-
-        using (var client = new Connection(boundAddress))
-        {
-          /// await client.CreateProxy()
-        }
-
-
-        // Previous Client Example
-        ////using (var client = new Connection(boundAddress))
-        ////{
-        ////  await client.ConnectAsync();
-        ////  Console.WriteLine("Client Connected!");
-        ////
-        ////  var proxy = client.CreateProxy<IMyService>("Test.BleAdapter", MyService.Path);
-        ////  var greeting = await proxy.SendCommandAsync("BLE");
-        ////
-        ////  Assert.AreEqual(greeting, "Hello BLE!");
-        ////}
-      }
-    }
+    ////[TestMethod]
+    ////public async Task CanInitializeAdapterTest()
+    ////{
+    ////  // Test Server
+    ////  var server = new ServerConnectionOptions();
+    ////  using (var connection = new Connection(server))
+    ////  {
+    ////    await connection.RegisterObjectAsync(new Adapter());
+    ////    var boundAddress = await server.StartAsync("tcp:host=localhost");
+    ////
+    ////    Console.WriteLine($"Server is listening at, {boundAddress}");
+    ////
+    ////    // Multi-client connections:
+    ////    //// for (int cli = 0; i < 2; i++) { .. create clients .. };
+    ////
+    ////    // 1. Create fake adapter for testing
+    ////    // 2. Connect to fake adapter
+    ////    //    var adapter = await BlueZManager.GetAdapterAsync("hci99");
+    ////    // 3. Get properties from FakeAdapterService
+    ////    // 4. Dispose objects.
+    ////            
+    ////
+    ////    // Previous Client Example
+    ////    ////using (var client = new Connection(boundAddress))
+    ////    ////{
+    ////    ////  await client.ConnectAsync();
+    ////    ////  Console.WriteLine("Client Connected!");
+    ////    ////
+    ////    ////  var proxy = client.CreateProxy<IMyService>("Test.BleAdapter", MyService.Path);
+    ////    ////  var greeting = await proxy.SendCommandAsync("BLE");
+    ////    ////
+    ////    ////  Assert.AreEqual(greeting, "Hello BLE!");
+    ////    ////}
+    ////  }
+    ////}
   }
 }
