f7ec92e874883b65412dabe150f2184ff6b94042
Damian Suess <djsuess@gmail.com>
2022-07-26 18:46:18 -0400

    Added helper for reading battery levels. And split up constants

---
+++ src/Linux.Bluetooth/Constants/BluezConstants.cs
@@ -0,0 +1,18 @@
+﻿namespace Linux.Bluetooth
+{
+  public static class BluezConstants
+  {
+    public const string DbusService = "org.bluez";
+
+    public const string AdapterInterface = "org.bluez.Adapter1";
+    public const string BatteryInterface = "org.bluez.Battery1";
+    public const string DeviceInterface = "org.bluez.Device1";
+    public const string GattServiceInterface = "org.bluez.GattService1";
+    public const string GattManagerInterface = "org.bluez.GattManager1";
+    public const string GattCharacteristicInterface = "org.bluez.GattCharacteristic1";
+    public const string GattDescriptorInterface = "org.bluez.GattDescriptor1";
+    public const string LeAdvertisementInterface = "org.bluez.LEAdvertisement1";
+    public const string LeAdvertisingManagerInterface = "org.bluez.LEAdvertisingManager1";
+  }
+
+}
