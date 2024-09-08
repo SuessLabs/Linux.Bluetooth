1c04223e46fbb4baeb4f46c8bb059bea310bdef2
Damian Suess <djsuess@gmail.com>
2024-09-08 13:11:48 -0400

    Advertisement and sample CLI

---
+++ samples/BleServerCli/Program.cs
@@ -0,0 +1,65 @@
+﻿using Linux.Bluetooth;
+using Tmds.DBus;
+
+Console.WriteLine("Linux.Bluetooth Server Example");
+
+using (var bluez = new DBusServer())
+{
+  // Connect to DBus
+  await bluez.InitializeAsync();
+
+  // Get BLE Adapter
+  var adapter = await GetDefaultAdapterAsync();
+  if (adapter is null)
+  {
+    Console.WriteLine("No BLE Adapters found.");
+    return;
+  }
+
+  // Turn on BLE Adapter
+  await adapter.SetPoweredAsync(true);
+
+  await CreateAdvertisementAsync();
+
+  Console.WriteLine("Press any key to quit");
+  Console.ReadLine();
+}
+
+static async Task<Adapter?> GetDefaultAdapterAsync()
+{
+  var adapters = await BlueZManager.GetAdaptersAsync();
+  if (adapters.Count == 0)
+    return null;
+
+  return adapters[0];
+}
+
+static async Task CreateAdvertisementAsync()
+{
+  var bluezAdv = new LEAdvertisement1Properties
+  {
+    Type = "",
+    ServiceUUIDs = ["3515A516-A069-41EF-9222-1D0343124680"],
+    LocalName = "My Linux.Bluetooth Device!",
+    Appearance = 0x80,
+    Discoverable = true,  // False for Broadcast
+    IncludeTxPower = true,
+  };
+
+  // TODO: Register with AdvertizeManager
+  return;
+}
+
+/// <summary>BlueZ DBus Server class.</summary>
+/// <remarks>Rename to BleServer and move methods into here.</remarks>
+class DBusServer : IDisposable
+{
+  public DBusServer() => Connection = new Connection(Address.System);
+
+  public async Task InitializeAsync() => await Connection.ConnectAsync();
+
+  public Connection Connection { get; }
+
+  public void Dispose() => Connection.Dispose();
+}
+
---
+++ src/Linux.Bluetooth/Advertisement.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Tmds.DBus;
+
+namespace Linux.Bluetooth
+{
+  public class Advertisement : ILEAdvertisement1, IDisposable
+  {
+    public ObjectPath ObjectPath => throw new NotImplementedException();
+
+    public void Dispose()
+    {
+      throw new NotImplementedException();
+    }
+
+    public Task<LEAdvertisement1Properties> GetAllAsync()
+    {
+      throw new NotImplementedException();
+    }
+
+    public Task<object> GetAsync(string prop)
+    {
+      throw new NotImplementedException();
+    }
+
+    public Task ReleaseAsync()
+    {
+      throw new NotImplementedException();
+    }
+
+    public Task SetAsync(string prop, object val)
+    {
+      throw new NotImplementedException();
+    }
+
+    public Task<IDisposable> WatchPropertiesAsync(Action<PropertyChanges> handler)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
