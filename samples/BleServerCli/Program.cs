e97f10860dbf175d5cb72b28754d8684809ffc67
Damian Suess <djsuess@gmail.com>
2025-03-16 09:28:23 -0400

    Bluetooth Server Client sample (9/4/24)

--- samples/BleServerCli/Program.cs
+++ samples/BleServerCli/Program.cs
@@ -1,65 +1,119 @@
-﻿using Linux.Bluetooth;
+﻿using System;
+using Linux.Bluetooth;
 using Tmds.DBus;
 
-Console.WriteLine("Linux.Bluetooth Server Example");
-
-using (var bluez = new DBusServer())
+public class Program
 {
-  // Connect to DBus
-  await bluez.InitializeAsync();
-
-  // Get BLE Adapter
-  var adapter = await GetDefaultAdapterAsync();
-  if (adapter is null)
+  private static async Task Main(string[] args)
   {
-    Console.WriteLine("No BLE Adapters found.");
-    return;
+    Console.WriteLine("Linux.Bluetooth Server Example");
+
+    using var bleServer = new BleGattServer();
+    // Connect to DBus
+    await bleServer.InitializeAsync();
+
+    // Get BLE Adapter
+    var adapter = await GetDefaultAdapterAsync();
+    if (adapter is null)
+    {
+      Console.WriteLine("No BLE Adapters found.");
+      return;
+    }
+
+    // Turn on BLE Adapter
+    await adapter.SetPoweredAsync(true);
+
+    // Make advertisement details for our server
+    var adv = bleServer.CreateAdvertisement(
+      "My Linux.Bluetooth Device!",
+      "3515A516-A069-41EF-9222-1D0343124680");
+
+    await bleServer.StartAdvertisingAsync();
+
+    Console.WriteLine("Press any key to quit");
+    Console.ReadLine();
   }
 
-  // Turn on BLE Adapter
-  await adapter.SetPoweredAsync(true);
-
-  await CreateAdvertisementAsync();
-
-  Console.WriteLine("Press any key to quit");
-  Console.ReadLine();
-}
-
-static async Task<Adapter?> GetDefaultAdapterAsync()
-{
-  var adapters = await BlueZManager.GetAdaptersAsync();
-  if (adapters.Count == 0)
-    return null;
-
-  return adapters[0];
-}
-
-static async Task CreateAdvertisementAsync()
-{
-  var bluezAdv = new LEAdvertisement1Properties
+  private static async Task<Adapter?> GetDefaultAdapterAsync()
   {
-    Type = "",
-    ServiceUUIDs = ["3515A516-A069-41EF-9222-1D0343124680"],
-    LocalName = "My Linux.Bluetooth Device!",
-    Appearance = 0x80,
-    Discoverable = true,  // False for Broadcast
-    IncludeTxPower = true,
-  };
+    var adapters = await BlueZManager.GetAdaptersAsync();
+    if (adapters.Count == 0)
+      return null;
 
-  // TODO: Register with AdvertizeManager
-  return;
+    return adapters[0];
+  }
 }
 
-/// <summary>BlueZ DBus Server class.</summary>
-/// <remarks>Rename to BleServer and move methods into here.</remarks>
-class DBusServer : IDisposable
+/// <summary>BlueZ D-Bus GATT Server class.</summary>
+/// <remarks>Move  methods into here.</remarks>
+public class BleGattServer : IDisposable
 {
-  public DBusServer() => Connection = new Connection(Address.System);
+  private const string Peripheral = "peripheral";
+
+  public BleGattServer() => Connection = new Connection(Address.System);
 
   public async Task InitializeAsync() => await Connection.ConnectAsync();
 
   public Connection Connection { get; }
 
   public void Dispose() => Connection.Dispose();
-}
 
+  public LEAdvertisement1Properties CreateAdvertisement(string name, string uuid)
+  {
+    var bluezAdv = new LEAdvertisement1Properties
+    {
+      Type = Peripheral,
+      ServiceUUIDs = [uuid],
+      LocalName = name,
+      Appearance = 0x80,
+      Discoverable = true,  // False for Broadcast
+      IncludeTxPower = true,
+    };
+
+    // TODO: Register with AdvertizeManager
+
+    return bluezAdv;
+  }
+
+  public async Task<GattService> CreateService(string uuid)
+  {
+    var gattService = new GattService(uuid);
+
+    return gattService;
+  }
+
+  /// <summary>Begin advertising our BLE Server.</summary>
+  /// <param name="advertisement"></param>
+  /// <returns></returns>
+  public async Task RegisterAdvertisement(LEAdvertisement1Properties advertisement)
+  {
+    await new AdvertisingManager.
+  }
+
+  public class GattService
+  {
+    public GattService(string uuid) => Uuid = uuid;
+
+    public string Uuid { get; private set; }
+  }
+
+  public class GattCharacteristic
+  {
+    public ushort Read = 1 << 0;
+    public ushort Write = 1 << 1;
+    public ushort Notify = 1 << 2;
+    public ushort Broadcast = 1 << 3;
+    public ushort Indicate = 1 << 4;
+    public ushort Write_NR = 1 << 5; // Write No-Response
+
+    public GattCharacteristic(string uuid, int properties)
+    {
+      Uuid = uuid;
+      Properties = properties;
+    }
+
+    public string Uuid { get; private set; }
+
+    public int Properties { get; private set; }
+  }
+}
